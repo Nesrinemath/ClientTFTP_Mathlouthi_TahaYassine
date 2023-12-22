77416ef375b7a04b2dfe535dd3ad799c78f22957
ensea <ensea@StudentLab>
2023-12-22 14:15:32 +0100

    question1

---
+++ Question1/gettftp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    // Check if the correct number of command-line arguments is provided
+    if (argc != 4) {
+        // Display an error message and exit the program with success status
+        printf("Wrong usage: gettftp filename host port\n");
+        exit(EXIT_SUCCESS);
+    }
+    // Extract command-line arguments
+    const char *filename = argv[1];
+    const char *host = argv[2];
+    const char *port = argv[3];
+
+    // Display information about the gettftp server
+    printf("gettftp Server: Filename: %s, Host: %s, Port: %s \n", filename, host, port);
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
